8d2c85d0320c1aa218c2a5c447919a888de69c25
Wissem Boujlida <w.boujlida@sage.local>
2026-02-28 11:40:50 +0100

    add @ip 8.32+

--- backend/src/main/java/tn/sage/rh/config/OpenApiConfig.java
+++ backend/src/main/java/tn/sage/rh/config/OpenApiConfig.java
@@ -28,11 +28,11 @@ import io.swagger.v3.oas.annotations.servers.Server;
         servers = {
                 @Server(
                         description = "Local ENV",
-                        url = "http://192.168.8.102:9000"
+                        url = "http://192.168.8.32:9000"
                 ),
                 @Server(
                         description = "PROD ENV",
-                        url = "https://192.168.2.102:3000"
+                        url = "https://192.168.8.32:3000"
                 )
         },
         security = {
